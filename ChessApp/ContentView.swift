673000e714e35574032bc40fac921a03f46e597a
David Crooks <crodave@gmail.com>
2020-06-03 20:59:00 +0100

    Initial Commit

---
+++ ChessApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChessApp
+//
+//  Created by David Crooks on 03/06/2020.
+//  Copyright © 2020 David Crooks. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
